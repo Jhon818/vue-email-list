0a237a7bb1deca6115554f72556ebac7362f988c
giov <pintogiovanni818@gmail.com>
2021-09-16 16:45:53 +0200

    primo push

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const app = new Vue ({
     mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then((response) => {
-           const result = response.date;
+           const result = response.data;
            console.log(result);
         });
     }
