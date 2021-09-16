dce09a03378ff8129968a77ae9ea8be3f111c0ef
giov <pintogiovanni818@gmail.com>
2021-09-16 17:41:54 +0200

    secondo push random email

--- js/main.js
+++ js/main.js
@@ -2,13 +2,29 @@ Vue.config.devtools = true;
 
 const app = new Vue ({
     el: "#root",
-    list: [],
-    data: {},
+    data: {
+        list: [],
+    },
+    methods: {
+        randomEmail() {
+            //    generare 10 email 
+            for (let index = 0; index < 10; index++) {
+                    let obj = JSON.parse('{ "success":true,"response":"becker.aleen@conroy.com" }')
+                    // console.log(obj)
+                    this.list.push(obj);
+                  
+            }
+            console.log(this.list)
+        }
+
+
+    },
+
     mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then((response) => {
-           const result = response.data;
-           console.log(result);
+           result = response.data;
+           console.log(this.randomEmail());
         });
     }
 });
